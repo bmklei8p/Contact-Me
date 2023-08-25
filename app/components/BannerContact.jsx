8ada39efc4712f3a17149cbc102d7128c60ba3f0
bryan <bmklei8p@gmail.com>
2023-08-25 11:27:18 -0400

    updated color scheme, finished contactme banner form, and updated to incorporate sidebar

--- app/components/BannerContact.jsx
+++ app/components/BannerContact.jsx
@@ -13,10 +13,9 @@ const BannerContact = () => {
   const [emailSentSuccessfully, setEmailSentSuccessfully] = useState(false);
   const [emailSentFailed, setEmailSentFailed] = useState(false);
 
-
   const onSubmit = async (data, e) => {
     e.target.reset();
-    data.API_KEY=process.env.NEXT_PUBLIC_EMAIL_API_KEY
+    data.API_KEY = process.env.NEXT_PUBLIC_EMAIL_API_KEY;
     try {
       const response = await axios.post(
         "https://portfolio-email-service.azurewebsites.net/submit-form",
@@ -33,72 +32,114 @@ const BannerContact = () => {
       console.error("An error occurred while sending the email", error);
       setEmailSentFailed(true);
     }
-  }
-
+  };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="h-[100vh] w-full min-w-fit flex flex-col items-center justify-center">
-        <div className="w-[90%] bg-gray-100 py-10 flex flex-wrap justify-center relative">
-          <h1 className="text-3xl mb-4 absolute left-12">
-            <div className="flex">
-              {/* <strong><span className="border-b-2 border-black">C</span>ontact</strong> me */}
-              <span><strong className="border-b-2 border-black">C</strong><strong>ontact</strong> me</span>
-            </div>
-          </h1>
-          <div className="w-[90%] bg-white py-6 px-6 mt-12 shadow-2xl overflow-hidden ">
-            <div className="flex flex-col gap-y-8">
-              <div>
-                <input
-                  type="text"
-                  className="bg-white border-2 border-gray-400 px-4 py-1 "
-                  placeholder="Reciever email"
-                  {...register("recieverEmail", { required: true })}
-                />
-                {errors.name && errors.name.type === "required" && (
-                  <span className="invalid-feedback">Name is required</span>
-                )}
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="h-[100vh] w-full min-w-fit flex flex-col items-center justify-center">
+          <div className="w-[90%] bg-offset py-10 flex flex-wrap justify-center relative">
+            {emailSentSuccessfully && (
+              <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center">
+                <div className="bg-background p-8 rounded-md">
+                  <h1 className="text-2xl font-bold mb-4 text-center">
+                    Email sent successfully
+                  </h1>
+                  <p className="text-lg mb-8">
+                    Thank you for contacting me. I will get back to you as soon
+                    as possible.
+                  </p>
+                  <button
+                    className="px-4 py-2 mt-4 flex justify-center w-full text-lg font-medium text-white bg-blue-500 border border-transparent rounded-md hover:bg-blue-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                    onClick={() => setEmailSentSuccessfully(false)}
+                  >
+                    Close
+                  </button>
+                </div>
               </div>
-              <span className="flex flex-col gap-y-8 gap-x-8 xl:flex-row">
-                <input
-                  className="bg-white border-2 border-gray-400 px-4 py-1 "
-                  type="text"
-                  placeholder="Name"
-                  {...register("senderName", { required: true })}
-                />
-                <input
-                  className="bg-white border-2 border-gray-400 px-4 py-1 "
-                  type="text"
-                  placeholder="Sender email"
-                  {...register("senderEmail", { required: true })}
-                />
-              </span>
-              <div>
-                <input
-                  className="bg-white border-2 border-gray-400 px-4 py-1 "
-                  type="text"
-                  placeholder="Subject"
-                  {...register("subject", { required: true })}
-                />
+            )}
+            {emailSentFailed && (
+              <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center">
+                <div className="bg-background p-8 rounded-md">
+                  <h1 className="text-2xl font-bold mb-4">
+                    Email sending failed
+                  </h1>
+                  <p className="text-lg">
+                    Please try again. If you continue to see this message,
+                    please contact me via linkedin.
+                  </p>
+                  <button
+                    className="px-4 py-2 mt-4 text-lg font-medium text-white bg-blue-500 border border-transparent rounded-md hover:bg-blue-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                    onClick={() => setEmailSentSuccessfully(false)}
+                  >
+                    Close
+                  </button>
+                </div>
               </div>
-              <div className="">
-                <input
-                  className="bg-contentBackground w-full h-24 border-2 border-gray-400 px-4"
-                  type="text"
-                  placeholder="Message"
-                  {...register("comment", { required: true })}
-                />
+            )}
+            <h1 className="text-3xl mb-4 absolute left-12">
+              <div className="flex">
+                {/* <strong><span className="border-b-2 border-black">C</span>ontact</strong> me */}
+                <span>
+                  <strong className="border-b-2 border-black">C</strong>
+                  <strong>ontact</strong> me
+                </span>
               </div>
-              <div>
-                <button className="min-w-fit px-4 py-2 text-lg font-medium text-white bg-blue-500 border border-transparent rounded-md hover:bg-blue-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500">
-                  Send your message
-                </button>
+            </h1>
+            <div className="w-[90%] bg-contrast py-6 px-6 mt-12 shadow-2xl overflow-hidden ">
+              <div className="flex flex-col gap-y-8">
+                <div>
+                  <input
+                    type="text"
+                    className="bg-contrast border-2 border-gray-400 px-4 py-1 "
+                    placeholder="Reciever email"
+                    {...register("recieverEmail", { required: true })}
+                  />
+                  {errors.name && errors.name.type === "required" && (
+                    <span className="invalid-feedback">Name is required</span>
+                  )}
+                </div>
+                <span className="flex flex-col gap-y-8 gap-x-8 xl:flex-row">
+                  <input
+                    className="bg-contrast border-2 border-gray-400 px-4 py-1 "
+                    type="text"
+                    placeholder="Name"
+                    {...register("senderName", { required: true })}
+                  />
+                  <input
+                    className="bg-contrast border-2 border-gray-400 px-4 py-1 "
+                    type="text"
+                    placeholder="Sender email"
+                    {...register("senderEmail", { required: true })}
+                  />
+                </span>
+                <div>
+                  <input
+                    className="bg-contrast border-2 border-gray-400 px-4 py-1 "
+                    type="text"
+                    placeholder="Subject"
+                    {...register("subject", { required: true })}
+                  />
+                </div>
+                <div className="">
+                  <input
+                    className="bg-contrast w-full h-24 border-2 border-gray-400 px-4"
+                    type="text"
+                    placeholder="Message"
+                    {...register("comment", { required: true })}
+                  />
+                </div>
+                <div>
+                  <button className="min-w-fit px-4 py-2 text-lg font-medium text-white bg-blue-500 border border-transparent rounded-md hover:bg-blue-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500">
+                    Send your message
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </>
   );
 };
 export default BannerContact;
--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -5,6 +5,8 @@ import Link from "next/link";
 import { FiSun, FiMoon } from "react-icons/fi";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 import { MdEmail } from "react-icons/md";
+import { RiMenuFill } from "react-icons/ri";
+import Sidebar from "./Sidebar";
 
 const Header = () => {
   const { theme, setTheme } = useTheme();
@@ -20,44 +22,56 @@ const Header = () => {
   }, []);
 
   return (
-    //  desktop header
-    <div className="flex flex-row bg-headerBackground text-white py-4 px-4 justify-between md:px-20">
-      <div className="flex items-center">
-        <Link href="/">
-          <div className="flex flex-row items-center align-middle gap-2">
-            <MdEmail size={30} />
-            <h2 className="text-2xl font-semibold">Contact Me</h2>
+    <>
+      <div className="flex flex-row bg-headerBackground text-white py-4 px-4 justify-between md:px-20">
+        <div className="flex items-center">
+          <Link href="/">
+            <div className="flex flex-row items-center align-middle gap-2">
+              <MdEmail size={30} />
+              <h2 className="text-2xl font-semibold">Contact Me</h2>
             </div>
           </Link>
-      </div>
-      <div className="flex flex-row gap-4">
-        {session?.user ? (
-          <button type="button" onClick={() => signOut()} className="min-w-fit">
-            <p className="text-lg">Sign Out</p>
+        </div>
+        <div className="flex flex-row gap-4">
+          {session?.user ? (
+            <button
+              type="button"
+              onClick={() => signOut()}
+              className="min-w-fit"
+            >
+              <p className="text-lg">Sign Out</p>
+            </button>
+          ) : (
+            <>
+              {providers &&
+                Object.values(providers).map((provider) => (
+                  <button
+                    type="button"
+                    key={provider.name}
+                    onClick={() => signIn(provider.id)}
+                    className="min-w-fit px-4 py-2 text-lg font-medium text-white bg-blue-500 border border-transparent rounded-md hover:bg-blue-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                  >
+                    <p>Sign In</p>
+                  </button>
+                ))}
+            </>
+          )}
+          <button
+            className="sidebar-link-block"
+            onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+          >
+            {theme === "light" ? <FiMoon size={25} /> : <FiSun size={25} />}
           </button>
-        ) : (
-          <>
-            {providers &&
-              Object.values(providers).map((provider) => (
-                <button
-                  type="button"
-                  key={provider.name}
-                  onClick={() => signIn(provider.id)}
-                  className="min-w-fit px-4 py-2 text-lg font-medium text-white bg-blue-500 border border-transparent rounded-md hover:bg-blue-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
-                >
-                  <p>Sign In</p>
-                </button>
-              ))}
-          </>
-        )}
-        <button
-          className="sidebar-link-block"
-          onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-        >
-          {theme === "light" ? <FiMoon size={25} /> : <FiSun size={25} />}
-        </button>
+          <button>
+            <RiMenuFill
+              size={30}
+              onClick={() => setToggleDropDown((prev) => !prev)}
+            />
+          </button>
+        </div>
       </div>
-    </div>
+      {toggleDropDown && <Sidebar />}
+    </>
   );
 };
 
