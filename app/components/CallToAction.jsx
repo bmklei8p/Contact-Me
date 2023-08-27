9bde4372ea8a3816bb3fd2f515c9a588ad910094
bryan <bmklei8p@gmail.com>
2023-08-27 17:03:42 -0400

    updated positioning of ui/ux elements on call to action but animation not yet complete

--- app/components/CallToAction.jsx
+++ app/components/CallToAction.jsx
@@ -1,16 +1,18 @@
 import EmailAnimation from "./EmailAnimation";
+import free from "../../public/no_resel.svg";
+import Image from "next/image";
+import { MdOutlineMarkEmailUnread } from "react-icons/md";
 
 const CallToAction = () => {
   return (
     <>
-      <div className="w-full md:w-[85%] flex flex-col items-center">
+      <div className="w-full xl:w-[85%] flex flex-col items-center">
         <h1 className="text-5xl pt-10 md:pt-20 font-bold pb-5 border-b-2 border-gray-400 w-full text-center">
           Contact Me
         </h1>
       </div>
-      <div className="w-full md:w-[85%] px-4 md:px-0 flex flex-col mt-8 md:mt-12 relative">
-        <div className="w-full flex justify-center lg:justify-start">
-          <EmailAnimation />
+      <div className="w-full xl:w-[85%] px-4 md:px-0 flex flex-col mt-8 md:mt-12 relative">
+        <div className="w-full flex flex-col lg:flex-row lg:gap-x-8 justify-center xl:justify-start">
           <div className="flex flex-col text-center">
             <h3 className="text-2xl md:text-3xl italic font-bold">
               Fastest and simplest
@@ -20,25 +22,35 @@ const CallToAction = () => {
               way to get your messages
             </h3>
           </div>
+          <div className="flex justify-center mt-4 relative">
+            <MdOutlineMarkEmailUnread className="text-blue-500" size={60}/>
+            <EmailAnimation />
+          </div>
         </div>
-        <div className="w-full flex justify-center lg:justify-end">
+        <div className="w-full flex justify-center xl:justify-end">
           <div className="text-2xl md:text-3xl mt-32 md:mt-44 flex flex-col text-center items-center">
-            <h2>Aquire your <strong>API Key</strong></h2>
+            <h2>
+              Aquire your <strong>API Key</strong>
+            </h2>
             <h3>and start getting contacted</h3>
-            <button className="min-w-fit mt-4 px-4 py-2 text-lg font-medium text-white bg-blue-500 border border-transparent rounded-md hover:bg-blue-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500">
+            <button className="min-w-fit mt-6 px-4 py-2 text-lg font-medium text-white bg-blue-500 border border-transparent rounded-md hover:bg-blue-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500">
               Get started
             </button>
           </div>
         </div>
-        <div className="w-full flex justify-center lg:justify-start">
-          <div className="text-2xl md:text-3xl mt-32 md:mt-44 flex flex-col text-center">
-            <h3>
-              <strong>100% free</strong> email
-            </h3>
-            <h3>software as a service (Saas)</h3>
+        <div className="w-full flex justify-center xl:justify-start">
+          <div className="text-2xl md:text-3xl mt-32 md:mt-44 flex flex-col lg:flex-row md:gap-x-8 text-center">
+            <div>
+              <h3>
+                <strong>100% free</strong> email
+              </h3>
+              <h3>Software as a Service</h3>
+            </div>
+            <div className="flex justify-center mt-4">
+              <Image src={free} alt="free" height={75} width={75} className="fill-blue-500" />
+            </div>
           </div>
         </div>
-      
       </div>
     </>
   );
--- app/components/EmailAnimation.jsx
+++ app/components/EmailAnimation.jsx
@@ -1,19 +1,16 @@
 "use client";
-import { MdOutlineMarkEmailUnread } from "react-icons/md";
 import { SiMinutemailer } from "react-icons/si";
 import { useState, useEffect } from "react";
 
 const EmailAnimation = () => {
   const [emailFlyingAnimationComplete, setEmailFlyingAnimationComplete] =
     useState(false);
-  const [triggerExplosionAnimation, setTriggerExplosionAnimation] =
-    useState(false);
+
 
   useEffect(() => {
     const delayForAnimation = (event) => {
       setTimeout(() => {
         setEmailFlyingAnimationComplete(true);
-        setTriggerExplosionAnimation(true);
       }, 900);
     };
     delayForAnimation();
@@ -22,14 +19,6 @@ const EmailAnimation = () => {
   return (
     <>
       {/* desktop */}
-      <MdOutlineMarkEmailUnread
-        className={
-          emailFlyingAnimationComplete
-            ? "absolute right-10 text-blue-500 hidden lg:block"
-            : "hidden"
-        }
-        size={60}
-      />{" "}
       <div
         id="animate"
         className="w-16 h-16 absolute animate-email-flying-animation"
@@ -44,18 +33,7 @@ const EmailAnimation = () => {
         />
       </div>
       {/* mobile */}
-      <MdOutlineMarkEmailUnread
-        className={
-          emailFlyingAnimationComplete
-            ? "absolute top-24 text-blue-500 lg:hidden"
-            : "hidden"
-        }
-        size={60}
-      />{" "}
-      <div
-        id="animate"
-        className="w-16 h-16 absolute animate-email-flying-animation"
-      >
+      <div>
         <SiMinutemailer
           className={
             emailFlyingAnimationComplete === true
