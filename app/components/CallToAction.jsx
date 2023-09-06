4e7e058f32efe3c857c4a7d305f11003a083b002
bryan <bmklei8p@gmail.com>
2023-09-06 15:50:52 -0400

    updated call to action responsiveness

--- app/components/CallToAction.jsx
+++ app/components/CallToAction.jsx
@@ -1,48 +1,87 @@
 // import EmailAnimation from "./EmailAnimation";
 // import free from "../../public/no_resel.svg";
-// import Image from "next/image";
+import email from "../../public/email.png";
+
+import Image from "next/image";
 import Link from "next/link";
 import { MdOutlineMarkEmailUnread } from "react-icons/md";
 
 const CallToAction = () => {
   return (
     <>
-      <div className="w-full xl:w-[85%] flex flex-col items-center">
-        <h1 className="text-5xl pt-10 md:pt-20 font-bold pb-5 border-b-2 border-gray-400 w-full text-center">
+      <div className="w-full flex flex-col items-center mt-20">
+        <h1 className="text-5xl md:w-[90%] font-bold pb-5 border-b-2 border-gray-400 w-full text-center">
           Contact Me
         </h1>
       </div>
-      <div className="w-full xl:w-[85%] px-4 md:px-0 flex flex-col mt-8 md:mt-12 relative">
-        <div className="w-full flex flex-col lg:flex-row lg:gap-x-8 justify-center xl:justify-start">
-          <div className="flex flex-col text-center">
-            <h3 className="text-2xl md:text-3xl italic font-bold">
+      <div className="w-full xl:w-[85%] px-4 md:px-0 flex flex-col 2xl:hidden">
+        <div className="w-full flex flex-col lg:flex-row lg:gap-x-8 justify-center">
+          <div className="flex flex-col text-center mt-10">
+            <h3 className="text-3xl md:text-3xl italic font-bold">
               Fastest and simplest
             </h3>
-            <h3 className="text-2xl md:text-3xl mt-2">
+            <h3 className="text-3xl md:text-3xl mt-2">
               {" "}
               way to get your messages
             </h3>
           </div>
-          <div className="flex justify-center mt-4 relative">
-            <MdOutlineMarkEmailUnread className="text-blue-500" size={60}/>
-            {/* <EmailAnimation /> */}
+        </div>
+        <div className="flex justify-center">
+          <Image src={email} width={650} height={650} className="" />
+        </div>
+        <div className="w-full flex justify-center">
+          <div className="text-3xl flex flex-col text-center">
+            <h2>
+              Aquire your <strong>API Key</strong>
+            </h2>
+            <h3>and start getting contacted</h3>
+            <Link href={"/api_key"}>
+              <button className="min-w-fit mt-6 px-4 py-2 text-lg font-medium text-white bg-blue-500 border border-transparent rounded-md hover:bg-blue-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500">
+                Get started
+              </button>
+            </Link>
           </div>
         </div>
-        <div className="w-full flex justify-center xl:justify-end">
-        <div className="text-2xl md:text-3xl mt-32 md:mt-44 flex flex-col lg:flex-row md:gap-x-8 text-center">
+        <div className="w-full flex justify-center">
+          <div className="text-3xl flex flex-col text-center items-center mt-20">
             <div>
               <h3>
                 <strong>100% free</strong> email
               </h3>
               <h3>Software as a Service</h3>
             </div>
-            {/* <div className="flex justify-center mt-4 items-center">
-              <Image src={free} alt="free" height={75} width={75} className="fill-blue-500" />
-            </div> */}
           </div>
         </div>
-        <div className="w-full flex justify-center xl:justify-start">
-        <div className="text-2xl md:text-3xl mt-32 md:mt-44 flex flex-col text-center items-center">
+      </div>
+
+
+      <div className="w-full px-4 md:px-0 flex-col mt-8 md:mt-12 relative  hidden 2xl:flex">
+        <div className="w-full flex flex-col lg:flex-row lg:gap-x-8 justify-center xl:justify-center">
+          <div className="flex flex-col text-left w-[70%]">
+            <h3 className="text-3xl md:text-3xl italic font-bold">
+              Fastest and simplest
+            </h3>
+            <h3 className="text-3xl md:text-3xl mt-2">
+              {" "}
+              way to get your messages
+            </h3>
+          </div>
+        </div>
+        <div className="w-full flex justify-center">
+          <div className="text-3xl flex flex-row text-center">
+          <div className="flex justify-start">
+          <Image src={email} width={650} height={650} className="" />
+        </div>
+            <div className="flex flex-col text-center justify-center">
+              <h3>
+                <strong>100% free</strong> email
+              </h3>
+              <h3>Software as a Service</h3>
+            </div>
+          </div>
+        </div>
+        <div className="w-full flex justify-center">
+          <div className="text-3xl flex flex-col text-start w-[70%] items-center">
             <h2>
               Aquire your <strong>API Key</strong>
             </h2>
@@ -58,5 +97,57 @@ const CallToAction = () => {
     </>
   );
 };
+
+// const CallToAction = () => {
+//   return (
+//     <>
+// <div className="w-full flex flex-col items-center">
+//   <h1 className="text-5xl md:w-[90%] pt-10 md:pt-20 font-bold pb-5 border-b-2 border-gray-400 w-full text-center">
+//     Contact Me
+//   </h1>
+// </div>
+// <div className="w-full xl:w-[85%] px-4 md:px-0 flex flex-col mt-8 md:mt-12 relative gap-y-20">
+//   <div className="w-full flex flex-col lg:flex-row lg:gap-x-8 justify-center xl:justify-center">
+//     <div className="flex flex-col text-center">
+//       <h3 className="text-3xl md:text-3xl italic font-bold">
+//         Fastest and simplest
+//       </h3>
+//       <h3 className="text-3xl md:text-3xl mt-2">
+//         {" "}
+//         way to get your messages
+//       </h3>
+//     </div>
+//     <div className="flex justify-center mt-4 relative">
+//       {/* <MdOutlineMarkEmailUnread className="text-blue-500" size={60}/> */}
+//       {/* <EmailAnimation /> */}
+//     </div>
+//   </div>
+//   <div className="w-full flex justify-center">
+//     <div className="text-3xl mt-80 md:mt-48 lg:mt-80 flex flex-col text-center">
+//       <h2>
+//         Aquire your <strong>API Key</strong>
+//       </h2>
+//       <h3>and start getting contacted</h3>
+//       <Link href={"/api_key"}>
+//         <button className="min-w-fit mt-6 px-4 py-2 text-lg font-medium text-white bg-blue-500 border border-transparent rounded-md hover:bg-blue-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500">
+//           Get started
+//         </button>
+//       </Link>
+//     </div>
+//   </div>
+//   <div className="w-full flex justify-center">
+//     <div className="text-2xl md:text-3xl mt-20 md:mt-32 flex flex-col text-center items-center">
+//       <div>
+//         <h3>
+//           <strong>100% free</strong> email
+//         </h3>
+//         <h3>Software as a Service</h3>
+//       </div>
+//     </div>
+//   </div>
+// </div>
+//     </>
+//   );
+// };
 
 export default CallToAction;
--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,14 @@
 import BannerContact from "./components/BannerContact";
 import EmailAnimation from "./components/EmailAnimation";
 import CallToAction from "./components/CallToAction";
+import Image from "next/image";
+import email from "../public/email.png";
 
 export default function Home() {
   return (
     <div className="w-full h-5/6 overflow-hidden flex flex-col md:flex-row">
-      <div className="basis-1/2 flex flex-col items-center">
+      <div className="basis-1/2 flex flex-col items-center relative">
+        {/* <Image src={email} width={650} height={650} className=" absolute top-[20%] left-0  -z-50" /> */}
         <CallToAction />
       </div>
       <div className="basis-1/2">
